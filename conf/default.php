e9020489d387b66e0a36a694e4d1e44baabcfcaa
Ronan Viel <ronan.viel@orange.com>
2015-10-30 17:36:58 +0100

    Integrate conf files

---
+++ conf/default.php
@@ -0,0 +1,11 @@
+<?php
+/**
+ * Configuration defaults for the Jive Plugin
+ * 
+ * @license    GPL 2 (http://www.gnu.org/licenses/gpl-2.0.html)
+ * @author	   Ronan Viel <ronan.viel@orange.com>
+ */
+
+$conf['jiveServerURL']	= '!!not set!!';
+$conf['jiveServerUser']	= '!!not set!!';
+$conf['jiveServerPassword']	= '!!not set!!';
---
+++ conf/metadata.php
@@ -0,0 +1,12 @@
+<?php
+/**
+ * Metadata on Jive Plugin configuration for configuration manager plugin
+ *
+ * @license    GPL 2 (http://www.gnu.org/licenses/gpl-2.0.html)
+ * @author	   Ronan Viel <ronan.viel@orange.com>
+ */
+
+$meta['jiveServerURL']		= array('string');
+$meta['jiveServerUser']		= array('string');
+$meta['jiveServerPassword']	= array('password');
+ 
